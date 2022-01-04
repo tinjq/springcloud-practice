5ab41d583a48f2c55721f9c643e6689dc3792ba5
tinjq <1967694532@qq.com>
2022-01-04 21:13:05 +0800

    ribbon

--- provider-service/src/main/java/com/jqtin/scp/providerservice/ProviderServiceApp.java
+++ provider-service/src/main/java/com/jqtin/scp/providerservice/ProviderServiceApp.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
+// @EnableDiscoveryClient // 该注解可以不加，见 EurekaClientAutoConfiguration
 @SpringBootApplication
 public class ProviderServiceApp {
 
--- ribbon-consumer/src/main/java/com/jqtin/scp/ribbonconsumer/RibbonConsumerApp.java
+++ ribbon-consumer/src/main/java/com/jqtin/scp/ribbonconsumer/RibbonConsumerApp.java
@@ -1,5 +1,7 @@
 package com.jqtin.scp.ribbonconsumer;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -19,5 +21,10 @@ public class RibbonConsumerApp {
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public IRule iRule() {
+        return new RandomRule();
     }
 }
