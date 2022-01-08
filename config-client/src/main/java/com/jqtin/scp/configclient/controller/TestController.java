b28d9f2a0d63c87ef8df78910ab30bafdefe3bba
tinjq <1967694532@qq.com>
2022-01-08 20:52:41 +0800

    config: security cipher

--- config-client/src/main/java/com/jqtin/scp/configclient/controller/TestController.java
+++ config-client/src/main/java/com/jqtin/scp/configclient/controller/TestController.java
@@ -1,19 +1,24 @@
 package com.jqtin.scp.configclient.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RefreshScope
 @RestController
 public class TestController {
 
     @Value("${from}")
     private String from;
+    @Value("${spring.datasource.password:123}")
+    private String dataSourcePassword;
 
     @RequestMapping("/from")
     public String from() {
+        log.info("password:{}", dataSourcePassword);
         return this.from;
     }
 
